d13b74327a876221d91eae3fccd2e125102aa62c
Tod Thomson <todthomson@me.com>
2015-10-06 14:07:40 +1000

    adds the hello world to microservice #1

---
+++ Client.Microservice1.Service/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace Client.Microservice1.Service
+{
+    class Program
+    {
+        // ReSharper disable once FunctionNeverReturns
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Hello, Microservice1!");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
